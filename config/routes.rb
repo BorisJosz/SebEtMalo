2a69a742ce1b43d706b0b9c13a1cd2803264956e
Boris Josz <Boris.Josz@gmail.com>
2019-05-05 19:53:19 +0200

    Schedule page done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'schedule', to: 'pages#schedule'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
