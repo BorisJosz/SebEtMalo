fd9d9909517ae6a00e54aa522cb53da6be432952
Boris Josz <Boris.Josz@gmail.com>
2019-05-11 18:44:06 +0200

    finished Getting there + google maps integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'schedule', to: 'pages#schedule'
+  get 'getting_there', to: 'pages#getting_there'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
