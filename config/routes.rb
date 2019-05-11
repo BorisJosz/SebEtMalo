b7ff74289cb144c8a32f9253f25dc589072e2db5
Boris Josz <Boris.Josz@gmail.com>
2019-05-11 19:04:42 +0200

    Accomodations page done

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'schedule', to: 'pages#schedule'
   get 'getting_there', to: 'pages#getting_there'
+  get 'accomodation', to: 'pages#accomodation'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
