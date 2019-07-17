7aff5b7f139afdd46581e03514ac646fc7a012f7
Boris Josz <Boris.Josz@gmail.com>
2019-07-17 21:55:31 +0200

    added the env variables for smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,14 @@ Rails.application.configure do
 
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'www.maloetseb.be',
+    user_name:            ENV['GMAIL_ADDRESS'],
+    password:             ENV['GMAIL_APP_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true }
   config.action_mailer.default_url_options = { host: "www.maloetseb.be" }
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -70,7 +78,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
