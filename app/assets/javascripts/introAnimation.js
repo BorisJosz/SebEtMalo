3eadd9f1afbffefdd51c3ae7056a74f73936d232
Boris Josz <Boris.Josz@gmail.com>
2019-07-28 20:32:27 +0200

    removed cookie to prevend on load animation Home page T^T

--- app/assets/javascripts/introAnimation.js
+++ app/assets/javascripts/introAnimation.js
@@ -20,37 +20,4 @@ $(document).ready(function() {
 		onLoadAnimation(0);
 	}, 5000);
 
-	if (!readCookie("adSeen")) {
-		createCookie("adSeen", "1", 1000);
-	} else {
-		document.getElementById("introAnimation").classList.add("invisible");
-		$(".mainOpening").hide(0, function(){
-			$(".mainRainContainer").delay(0).css({
-				display: "block",
-				opacity: "1"
-			});
-		});
-	}
-
-	function createCookie(name, value, days) {
-        if (days) {
-            var date = new Date();
-            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-            var expires = "; expires=" + date.toGMTString();
-        } else var expires = "";
-		document.cookie = name + "=" + value + expires + "; path=/";
-    }
-
-    function readCookie(name) {
-        var nameEQ = name + "=";
-        var ca = document.cookie.split(';');
-        for (var i = 0; i < ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-        }
-        return null;
-    }
-	
-
 });
