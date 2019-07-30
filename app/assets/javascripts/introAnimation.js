6ddc00e0538faf0deb6fa79a2a199953640aed0c
Boris Josz <Boris.Josz@gmail.com>
2019-07-30 20:16:33 +0200

    custom animation bby

--- app/assets/javascripts/introAnimation.js
+++ app/assets/javascripts/introAnimation.js
@@ -1,23 +1,23 @@
-$(document).ready(function() {
-    function onLoadAnimation(animationDelay){
-		$(".layer1").delay(400 + animationDelay).hide("scale", {percent: 2000, direction: 'verticle'}, 2500 );
-		$(".layer2").delay(400 + animationDelay).hide("scale", {percent: 2000, direction: 'verticle'}, 5000 );
-		$(".layer3").delay(400 + animationDelay).hide("scale", {percent: 2000, direction: 'verticle'}, 8000 );
-		$(".invitationText").delay(2400 + animationDelay).fadeOut(300);
-		$(".layer4").delay(2400 + animationDelay).fadeOut(2000, function(){
-			$(".mainOpening").hide(50, function(){
-				$(".mainRainContainer").delay(100).css({
-					display: "block",
-    				opacity: "1"
-				});
-			});
-		});
-		localStorage.setItem("animationDone", true);
-	}
-	var animationDone = localStorage.getItem("animationDone");
+// $(document).ready(function() {
+//     function onLoadAnimation(animationDelay){
+// 		$(".layer1").delay(400 + animationDelay).hide("scale", {percent: 2000, direction: 'verticle'}, 2500 );
+// 		$(".layer2").delay(400 + animationDelay).hide("scale", {percent: 2000, direction: 'verticle'}, 5000 );
+// 		$(".layer3").delay(400 + animationDelay).hide("scale", {percent: 2000, direction: 'verticle'}, 8000 );
+// 		$(".invitationText").delay(2400 + animationDelay).fadeOut(300);
+// 		$(".layer4").delay(2400 + animationDelay).fadeOut(2000, function(){
+// 			$(".mainOpening").hide(50, function(){
+// 				$(".mainRainContainer").delay(100).css({
+// 					display: "block",
+//     				opacity: "1"
+// 				});
+// 			});
+// 		});
+// 		localStorage.setItem("animationDone", true);
+// 	}
+// 	var animationDone = localStorage.getItem("animationDone");
 	
-	setTimeout(function(){
-		onLoadAnimation(0);
-	}, 5000);
+// 	setTimeout(function(){
+// 		onLoadAnimation(0);
+// 	}, 5000);
 
-});
+// });
