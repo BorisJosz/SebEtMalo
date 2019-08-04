8b4ce893fa577012aa6f8de01afc8a1bd1b7b43e
Boris Josz <Boris.Josz@gmail.com>
2019-08-04 21:06:19 +0200

    fini??

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "form",  to: 'rsvp#question_1'
   get "formulaire" , to: 'rsvp#question_2'
+  get "invitation" , to: 'rsvp#question_3'
   post "answer_1", to: "rsvp#answer_1"
   post "answer_2", to: "rsvp#answer_2"
   root to: 'pages#home'
